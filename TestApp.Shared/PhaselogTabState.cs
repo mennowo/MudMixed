31743611741286689b3c330bef564e0f78d42d50
Menno van der Woude <mennowo@gmail.com>
2024-09-26 13:13:50 +0200

    Add project files.

---
+++ TestApp.Shared/PhaselogTabState.cs
@@ -0,0 +1,10 @@
+﻿namespace TestApp_2.Components.Models.Tabs
+{
+    public class PhaselogTabState : TabStateBase
+    {
+        public override string TabName => IntersectionName + " - fasenlog";
+
+        public int Position;
+
+    }
+}
