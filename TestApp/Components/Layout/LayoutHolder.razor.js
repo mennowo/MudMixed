72dedd4542d303aa8b9834f2c84bb89e430e63de
Menno van der Woude <mennowo@gmail.com>
2024-09-27 10:25:19 +0200

    Mixed mode with some little JS

---
+++ TestApp/Components/Layout/LayoutHolder.razor.js
@@ -0,0 +1,21 @@
+﻿export function HandleNavMenuToggled(isOpen) {
+    const layoutElement = document.querySelector(".mud-layout");
+    if (isOpen) {
+        layoutElement.classList.replace("mud-drawer-close-responsive-md-left", "mud-drawer-open-responsive-md-left");
+        console.log("Open");
+    } else {
+        layoutElement.classList.replace("mud-drawer-open-responsive-md-left", "mud-drawer-close-responsive-md-left");
+        console.log("Close");
+    }
+}
+
+export function HandleSecondListToggled(isOpen) {
+    const layoutElement = document.querySelector(".mud-layout");
+    if (isOpen) {
+        layoutElement.classList.replace("mud-drawer-close-responsive-md-right", "mud-drawer-open-responsive-md-right");
+        console.log("Open");
+    } else {
+        layoutElement.classList.replace("mud-drawer-open-responsive-md-right", "mud-drawer-close-responsive-md-right");
+        console.log("Close");
+    }
+}
